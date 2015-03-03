12ab6be6c2187c9360e690ab097c25c26097a3a0
LexHousingStudies <lfgr225@g.uky.edu>
2015-03-03 13:13:17 -0500

    added all folders, etc.

---
+++ FORECLOSED/NELex foreclosed 1999.js
@@ -0,0 +1,8 @@
+var foreclosed99 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "PVANUM": "15511800", "NUM1": "447", "NUM2": null, "DIR": null, "NAME": "RACE", "TYPE": "ST", "ADDRESS": "447 RACE ST", "UNIT": null, "TAX": "01", "CLASS": "M", "PVA_ACRE": 0.112500, "CONDO": null, "CAB_SLIDE": "1-079", "DATEENTRD": "2011\/04\/08", "residentia": "LVM PROPERTIES LLC", "resident_1": "447 RACE ST", "resident_2": "1999\/02\/04", "resident_3": null, "resident_4": 50000, "resident_5": 1963, "resident_6": 2800, "resident_7": 0.112500, "resident_8": "R-3-PLANNED NBHD RESIDENTIAL", "resident_9": "M", "resident10": "000-COMMERCIAL ENTIRE COUNTY", "resident11": "FORECLOSURE PROCEEDINGS" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -84.483201879684444, 38.046318676143891 ], [ -84.483521380705241, 38.046597420440811 ], [ -84.483479372592299, 38.04662664396767 ], [ -84.483473742377015, 38.046630574105798 ], [ -84.483423076947901, 38.046665868447889 ], [ -84.48310355559363, 38.046387104257207 ], [ -84.483163776215875, 38.046345176075064 ], [ -84.483201879684444, 38.046318676143891 ] ] ] } }
+]
+}
---
+++ FORECLOSED/NELex foreclosed 2000.js
@@ -0,0 +1,8 @@
+var foreclosed00 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "PVANUM": "14602700", "NUM1": "437", "NUM2": null, "DIR": null, "NAME": "SMITH", "TYPE": "ST", "ADDRESS": "437 SMITH ST", "UNIT": null, "TAX": "01", "CLASS": "R", "PVA_ACRE": 0.045900, "CONDO": null, "CAB_SLIDE": "1-014", "DATEENTRD": "2009\/12\/10", "residentia": "JACKSON DAVID SR", "resident_1": "437 SMITH ST", "resident_2": "2000\/10\/24", "resident_3": null, "resident_4": 20000, "resident_5": 1890, "resident_6": 476, "resident_7": 0.045900, "resident_8": "R-4-HIGH DENSITY APARTMENT", "resident_9": "R", "resident10": "134-PRICE", "resident11": "FORECLOSURE PROCEEDINGS" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -84.494917012277412, 38.056642806493002 ], [ -84.494944029534182, 38.056623287725749 ], [ -84.494968319966205, 38.056605791321836 ], [ -84.495202784894857, 38.056814125229842 ], [ -84.495178289230338, 38.056831781228404 ], [ -84.495151554306716, 38.056851215429099 ], [ -84.494917012277412, 38.056642806493002 ] ] ] } }
+]
+}
