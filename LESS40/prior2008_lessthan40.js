5df490cb8c9e2201be6636c8ff2821e26b386e03
LexHousingStudies <lfgr225@g.uky.edu>
2015-03-05 14:30:08 -0500

    adding less40 files

--- LESS40/prior2008_lessthan40.js
+++ LESS40/prior2008_lessthan40.js
@@ -1,4 +1,4 @@
-{
+var less40Prior = {
 "type": "FeatureCollection",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
                                                                                 
