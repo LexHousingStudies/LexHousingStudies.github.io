e99de4a4548346b36eed44aa2bae66e2badbbb2f
Laura Greenfield <lfgr225@g.uky.edu>
2015-03-12 12:05:11 -0400

    more

--- NELEX/MFC.2005.js
+++ NELEX/MFC.2005.js
@@ -1,4 +1,4 @@
-{
+var mfc2005 = {
 "type": "FeatureCollection",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
                                                                                 
--- NELEX/MFC.2007.js
+++ NELEX/MFC.2007.js
@@ -1,4 +1,4 @@
-{
+var mfc2007 = {
 "type": "FeatureCollection",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
                                                                                 
