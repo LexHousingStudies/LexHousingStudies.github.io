e8b1d4451bcbbed18b530221c89dc120473dc404
Laura Greenfield <lfgr225@g.uky.edu>
2015-03-12 12:08:04 -0400

    adding tfc

---
+++ NELEX/TFC.1995.js
@@ -0,0 +1,8 @@
+{
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "PVANUM": "16670900", "NUM1": "301", "NUM2": null, "DIR": "N", "NAME": "BROADWAY", "TYPE": null, "ADDRESS": "301 N BROADWAY", "UNIT": null, "TAX": "01", "CLASS": "M", "PVA_ACRE": 0.089100, "CONDO": null, "CAB_SLIDE": "A-114", "DATEENTRD": null, "Area": 0, "NELex_Conc": "301 N BROADWAY", "NELex_Co_1": "0.0891", "NELex_Co_2": "BLUEGRASS COUNTRY PROPERTIES LLC", "NELex_Co_3": "1", "NELex_Co_4": "10\/18\/1995", "NELex_Co_5": null, "NELex_Co_6": "190000", "NELex_Co_7": "1989", "NELex_Co_8": "R-4-HIGH DENSITY APARTMENT", "NELex_Co_9": "TRANSFER-TAX NOT PAID", "Tax Delinquent_YEAR": "1995" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -84.495345026053954, 38.052288565703329 ], [ -84.495443514182924, 38.052219535160148 ], [ -84.495659435606441, 38.052411046518742 ], [ -84.495553416937497, 38.052488989199603 ], [ -84.495345026053954, 38.052288565703329 ] ] ] } }
+]
+}
