18d383b8b38d08f7a3e66ab3c69af97c1b4c7a52
Laura Greenfield <lfgr225@GISLAB13.ad.uky.edu>
2015-04-07 19:35:08 -0400

    everything on map; need dif colors, fix layout

--- LANDLORDS/Donald and Ruth Schilling.js
+++ LANDLORDS/Donald and Ruth Schilling.js
@@ -1,4 +1,4 @@
-{
+var schilling = {
 "type": "FeatureCollection",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
                                                                                 
--- LANDLORDS/Marty Clifford.js
+++ LANDLORDS/Marty Clifford.js
@@ -1,4 +1,4 @@
-{
+var martyClif = {
 "type": "FeatureCollection",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
                                                                                 
