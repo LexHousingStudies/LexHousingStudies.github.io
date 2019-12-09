e2812c52296f2902f26b095d800ba66c988dd71f
Ryan Cooper <ryancooper729@gmail.com>
2019-12-09 11:01:56 -0500

    update to https

--- abandonment/app.js
+++ abandonment/app.js
@@ -41,8 +41,8 @@ map.attributionControl.addAttribution('<a href="http://fayettepva.com/" target="
 new L.Control.Zoom({ position: 'topright' }).addTo(map);
 
 // Add a basemap to the map object just created
-L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
   subdomains: 'abcd',
 }).addTo(map);
 
